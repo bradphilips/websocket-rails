891bcc88fca997d91e1d08220eaebb3e1504be10
Brad Philips <brad.t.philips@gmail.com>
2014-06-05 21:48:59 -0400

    Removed dependency of the load configuration.

--- lib/rails/config/routes.rb
+++ lib/rails/config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  load "#{Rails.root}/config/initializers/websocket_rails.rb"
-
   if Rails.version >= '4.0.0'
     match WebsocketRails.config.base_path, :to => WebsocketRails::ConnectionManager.new, via: [:get, :post]
   else
